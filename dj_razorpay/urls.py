20aae6c40427b2ece14081f9483dbc9da9a6e3d6
prabalgautam05 <prabalgautam05@gmail.com>
2025-04-09 05:03:03 +0530

    upgrade

---
+++ dj_razorpay/urls.py
@@ -0,0 +1,12 @@
+# dj_razorpay/urls.py
+
+from django.contrib import admin
+from django.urls import path
+from payment import views
+
+urlpatterns = [
+	path('', views.homepage, name='index'),
+	path('paymenthandler/', views.paymenthandler, name='paymenthandler'),
+	path('admin/', admin.site.urls),
+   
+]
---
+++ payment/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import doners_details
+
+# Register your models here.
+admin.site.register(doners_details)
